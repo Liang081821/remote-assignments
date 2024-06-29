9ac264f362a9e25e2251038de47c65662beecc01
張量 <changliang@Liang-2022.local>
2024-06-29 16:46:44 +0800

    third commit

--- Desktop/week2/week2.js
+++ Desktop/week2/week2.js
@@ -1,41 +1,39 @@
-
 // 第一題
 
-function calculate(args){
-
+function calculate(args) {
     let value;
-
-    if (args.op == '+'){
-    value = args.n1 + args.n2;
-    } 
-    else if(args.op == '-'){
-     value = args.n1 - args.n2;}
-    else{ value = 'Not supported';}
-    
-    return value;
+  
+    if (args.op == "+") {
+      value = args.n1 + args.n2;
+    } else if (args.op == "-") {
+      value = args.n1 - args.n2;
+    } else {
+      value = "Not supported";
     }
-   
-// 第二題
-
-function max(numbers){
-
+  
+    return value;
+  }
+  
+  // 第二題
+  
+  function max(numbers) {
     let maxnumber = numbers[0];
-    
-    for ( let i = 0; i < numbers.length; i++){
-    if(numbers[i] > maxnumber){
-        maxnumber  =  numbers[i];
-        }
+  
+    for (let i = 1; i < numbers.length; i++) {
+      if (numbers[i] > maxnumber) {
+        maxnumber = numbers[i];
+      }
     }
     return maxnumber;
-}
-
-// 第三題
-function calculate(data) {
-    let sum = 0 ; //初始化 sum
-    for (let i =0; i< data.products.length ; i++){
-    sum += data.products[i].price
+  }
+  
+  // 第三題
+  function calculate(data) {
+    let sum = 0; //初始化 sum
+    for (let i = 0; i < data.products.length; i++) {
+      sum += data.products[i].price;
     }
-    let total= sum*(1-data.discount);
+    const total = sum * (1 - data.discount);
     return total;
-    }
-    
+  }
+  
